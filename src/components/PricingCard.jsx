23e452fe44df2817b1f0d4b5b53dbd1bb75dac8e
ozzuleyha <ozzuleyha2@gmail.com>
2023-12-28 11:28:42 +0300

    Added Basic Carts for pricing plans

---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,30 @@
+import { CiCircleCheck } from "react-icons/ci";
+
+const PricingCard = ({header, description, price, features }) => {
+    return (
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{header}</div>
+                <p class="text-gray-700 text-base">
+                    {description}
+                </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <p class="text-gray-700 text-base">
+                    {price}
+                </p>
+            </div>
+
+                {features.map((feature) => ( 
+                    <div>
+                        <CiCircleCheck sx={{fontSize: "50px"}} />
+                        <p class="text-gray-700 text-base">
+                            {feature.description}
+                        </p>
+                    </div>
+                ))}
+            </div>
+    );
+};
+
+export default PricingCard;
